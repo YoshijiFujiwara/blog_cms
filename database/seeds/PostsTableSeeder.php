11f261848f5d602c59275ef809fe188a3f44421f
YoshijiFujiwara <yfbiology2@gmail.com>
2018-08-18 16:07:31 +0900

    factory

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+//        // reset the posts table
+//        DB::table('posts')->truncate();
+//
+//        // generate 10 dummy posts data
+//        $posts = [];
+//        for ($i = 1; $i <= 10; $i++)
+//        {
+//            $posts[] = [
+//                'author_id' => rand(1,3),
+//                ''
+//            ];
+//        }
+//
+//        DB::table('posts')->insert();
+    }
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+//        // reset the users table
+//        DB::table('users')->truncate();
+//
+//        // generate 3 users/author
+//        DB::table('users')->insert([
+//            [
+//                'name' => "John Doe",
+//                'email' => "johndoe@test.com",
+//                'password' => bcrypt('secret')
+//            ],
+//            [
+//                'name' => "John Doe2",
+//                'email' => "johndoe2@test.com",
+//                'password' => bcrypt('secret')
+//            ],
+//            [
+//                'name' => "John Doe super",
+//                'email' => "johndoesuper@test.com",
+//                'password' => bcrypt('secret')
+//            ]
+//        ]);
+    }
+}
